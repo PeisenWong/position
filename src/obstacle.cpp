fce54664e97276da4f053426f144b678d86e615d
PeisenWong <wongpeisen@gmail.com>
2023-01-13 02:07:57 +0800

    nvdkvn

--- src/obstacle.cpp
+++ src/obstacle.cpp
@@ -67,53 +67,53 @@ void ObstacleCallback(const obstacle_detector::Obstacles obs)
         ROS_INFO("Bruh, no param");
     }
 
-    if(counts > 0 && counts <= 10)
-    {
-        memcpy(&sending[0], &ack, 1);
-        memcpy(&sending[1], &counts, 4);
+//     if(counts > 0 && counts <= 10)
+//     {
+//         memcpy(&sending[0], &ack, 1);
+//         memcpy(&sending[1], &counts, 4);
 
-#ifndef SERIALLIB
-        do
-        {
-            send(s, sending, 5, MSG_WAITALL);
-        } while (recv(s, receive, 4, MSG_WAITALL) < 0);
-#else
-        serial.writeBytes(sending, 5);
-        serial.readBytes(receive, 4, 100);
-#endif
+// #ifndef SERIALLIB
+//         do
+//         {
+//             send(s, sending, 5, MSG_WAITALL);
+//         } while (recv(s, receive, 4, MSG_WAITALL) < 0);
+// #else
+//         serial.writeBytes(sending, 5);
+//         serial.readBytes(receive, 4, 100);
+// #endif
         
-        memcpy(&received_counts, &receive, 4);
+//         memcpy(&received_counts, &receive, 4);
 
-        if(received_counts != counts)
-            ROS_INFO("U fked up, received %d", received_counts);
-        else
-            ROS_INFO("Received %d", received_counts);
+//         if(received_counts != counts)
+//             ROS_INFO("U fked up, received %d", received_counts);
+//         else
+//             ROS_INFO("Received %d", received_counts);
 
-        for(int i = 0; i < PoleList.size(); i++)
-        {
-            memcpy(&sending[i * 12], &PoleList.at(i).x, 4);
-            memcpy(&sending[4 + i * 12], &PoleList.at(i).y, 4);
-            memcpy(&sending[8 + i * 12], &PoleList.at(i).distance, 4);
-        }
-#ifndef SERIALLIB
-        do
-        {
-            send(s, sending, PoleList.size() * 3 * 4, MSG_WAITALL);
-        } while (recv(s, receive, PoleList.size() * 3 * 4, MSG_WAITALL) < 0);
-#else
-        serial.writeBytes(sending, PoleList.size() * 3 * 4);
-        serial.readBytes(receive, PoleList.size() * 3 * 4, 100);
-#endif
-        for(int i = 0; i < received_counts; i++)
-        {
-            memcpy(&received_x, &receive[i * 12], 4);
-            memcpy(&received_y, &receive[i * 12 + 4], 4);
-            memcpy(&received_distance, &receive[i * 12 + 8], 4);
-            ROS_INFO("Received X: %.2f Y: %.2f Dist: %.2f", received_x, received_y, received_distance);
-        }
-        ROS_INFO("Done\n");
-        PoleList.clear();
-    }
+//         for(int i = 0; i < PoleList.size(); i++)
+//         {
+//             memcpy(&sending[i * 12], &PoleList.at(i).x, 4);
+//             memcpy(&sending[4 + i * 12], &PoleList.at(i).y, 4);
+//             memcpy(&sending[8 + i * 12], &PoleList.at(i).distance, 4);
+//         }
+// #ifndef SERIALLIB
+//         do
+//         {
+//             send(s, sending, PoleList.size() * 3 * 4, MSG_WAITALL);
+//         } while (recv(s, receive, PoleList.size() * 3 * 4, MSG_WAITALL) < 0);
+// #else
+//         serial.writeBytes(sending, PoleList.size() * 3 * 4);
+//         serial.readBytes(receive, PoleList.size() * 3 * 4, 100);
+// #endif
+//         for(int i = 0; i < received_counts; i++)
+//         {
+//             memcpy(&received_x, &receive[i * 12], 4);
+//             memcpy(&received_y, &receive[i * 12 + 4], 4);
+//             memcpy(&received_distance, &receive[i * 12 + 8], 4);
+//             ROS_INFO("Received X: %.2f Y: %.2f Dist: %.2f", received_x, received_y, received_distance);
+//         }
+//         ROS_INFO("Done\n");
+//         PoleList.clear();
+//     }
 }
 
 int main(int argc, char** argv)
