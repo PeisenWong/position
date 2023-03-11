24833a8e0a288ff3621f970d676eaf1bec1fabbf
CHAN TAI JIE <chantaijie@gmail.com>
2023-03-11 22:04:36 +0800

    convert to convention coordinates

--- src/obstacle.cpp
+++ src/obstacle.cpp
@@ -77,7 +77,7 @@ void ObstacleCallback(const obstacle_detector::Obstacles obs)
             {
                 pole.x = circle.center.x;
                 pole.y = circle.center.y;
-                auto temp = pole.x;
+                double temp = pole.x;
              //  pole.x = pole.y*-1;
                 pole.y = temp ;
                 
